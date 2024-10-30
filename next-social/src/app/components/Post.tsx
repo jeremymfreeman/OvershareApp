078673b5f7c6230f19a643839a9f69e6987e890f
CalebVR5 <155918401+CalebVR5@users.noreply.github.com>
2024-10-29 22:12:33 -0700

    1:21

--- next-social/src/app/components/Post.tsx
+++ next-social/src/app/components/Post.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Comments from './Comments'
 
 const Post = () => {
     return (
@@ -27,9 +28,24 @@ const Post = () => {
                 </p>
             </div>
             {/* INTERACTION */}
-            <div className="">
-
+            <div className="flex items-center justify-between text-sm my-4">
+                <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
+                    <Image src="/like.png" width={16} height={16} alt="" className="cursor-pointer"/>
+                    <span className="text-gray-300">|</span>
+                    <span className="text-gray-500">123<span className="hidden md:inline"> Likes</span></span>
+                </div>
+                <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
+                    <Image src="/comment.png" width={16} height={16} alt="" className="cursor-pointer"/>
+                    <span className="text-gray-300">|</span>
+                    <span className="text-gray-500">123<span className="hidden md:inline"> Comments</span></span>
+                </div>
+                <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
+                    <Image src="/share.png" width={16} height={16} alt="" className="cursor-pointer"/>
+                    <span className="text-gray-300">|</span>
+                    <span className="text-gray-500">123<span className="hidden md:inline"> Shares</span></span>
+                </div>
             </div>
+            <Comments/>
         </div>
     )
 }
