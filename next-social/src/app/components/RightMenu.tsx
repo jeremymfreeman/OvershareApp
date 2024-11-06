121b883e5b87af6c01e7c0beaa2d778636dcfe5c
CalebVR5 <155918401+CalebVR5@users.noreply.github.com>
2024-11-05 16:36:25 -0800

    2:58

--- next-social/src/app/components/RightMenu.tsx
+++ next-social/src/app/components/RightMenu.tsx
@@ -1,15 +1,16 @@
+import { User } from "@prisma/client"
 import Ad from "./Ad"
 import Birthdays from "./Birthdays"
 import FriendRequests from "./FriendRequests"
 import UserInfoCard from "./UserInfoCard"
 import UserMediaCard from "./UserMediaCard"
 
-const RightMenu = ({ userId }: {userId?: string}) => {
+const RightMenu = ({ user }: {user?: User}) => {
     return (
         <div className="flex flex-col gap-6">
-            {userId ? (<>
-                <UserInfoCard userId={userId}/>
-                <UserMediaCard userId={userId}/>
+            {user ? (<>
+                <UserInfoCard user={user}/>
+                <UserMediaCard user={user}/>
             </>) : null}
             <FriendRequests />
             <Birthdays />
--- next-social/src/app/components/UserInfoCard.tsx
+++ next-social/src/app/components/UserInfoCard.tsx
@@ -1,7 +1,17 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { User } from '@prisma/client'
 
-const UserInfoCard = ({userId}:{userId:string}) => {
+const UserInfoCard = ({user}:{user:User}) => {
+
+    const createdAtDate = new Date(user.createdAt);
+    const formattedDate = createdAtDate.toLocaleString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+    });
+
+
     return (
         <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4">
         {/* TOP */}
@@ -12,41 +22,39 @@ const UserInfoCard = ({userId}:{userId:string}) => {
         {/* BOTTOM */}
         <div className="flex flex-col gap-4 text-gray-500">
             <div className="flex items-center gap-2">
-                <span className="text-xl text-black">John Doe</span>
-                <span className="text-sm">@johndoe55</span>
+                <span className="text-xl text-black">{(user.name && user.surname) ? user.name + " " + user.surname : user.username}</span>
+                <span className="text-sm">@{user.username}</span>
             </div>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac
-                metus massa. Ut congue, felis in luctus lacinia, sapien turpis
-                tristique libero.
-            </p>
-            <div className="flex items-center gap-2">
+            {user.description && <p>
+                {user.description}
+            </p>}
+            {user.city && <div className="flex items-center gap-2">
                 <Image src="/map.png" alt="" width={16} height={16}
                 />
-                <span>Living in <b>Los Angeles</b></span>
-            </div>
-            <div className="flex items-center gap-2">
+                <span>Living in <b>{user.city}</b></span>
+            </div>}
+            {user.school && <div className="flex items-center gap-2">
                 <Image src="/school.png" alt="" width={16} height={16}
                 />
-                <span>Went to <b>Los Angeles High School</b></span>
-            </div>
-            <div className="flex items-center gap-2">
+                <span>Went to <b>{user.school}</b></span>
+            </div>}
+            {user.work && <div className="flex items-center gap-2">
                 <Image src="/work.png" alt="" width={16} height={16}
                 />
-                <span>Working at <b>Los Angeles Department of Sanitation</b></span>
-            </div>
+                <span>Working at <b>{user.work}</b></span>
+            </div>}
             <div className="flex items-center justify-between">
-                <div className="flex gap-1 items-center">
+                {user.website && <div className="flex gap-1 items-center">
                     <Image src="/link.png" alt="" width={16} height={16}
                     />
-                    <Link href="https://www.google.com/webhp" className="text-blue-500 font-medium">
-                        Find me on Google
+                    <Link href={user.website} className="text-blue-500 font-medium">
+                        {user.website}
                     </Link>
-                </div>
+                </div>}
                 <div className="flex gap-1 items-center">
                     <Image src="/date.png" alt="" width={16} height={16}
                     />
-                    <span>Joined July 2021</span>
+                    <span>Joined {formattedDate}</span>
                 </div>
             </div>
             <button className="bg-blue-500 text-white text-sm rounded-md p-2">Follow</button>
