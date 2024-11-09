419b732fb1f1afc519b2748121b7091692e4a9eb
CalebVR5 <155918401+CalebVR5@users.noreply.github.com>
2024-11-09 11:37:19 -0800

    4:40

--- next-social/src/app/components/feed/Post.tsx
+++ next-social/src/app/components/feed/Post.tsx
@@ -1,30 +1,32 @@
 import Image from 'next/image'
 import Comments from './Comments'
+import { Post as PostType, User } from '@prisma/client'
 
-const Post = () => {
+
+type FeedPostType = PostType & {user:User} & {likes:[{userId:string}]} & {_count:{comments:number}}
+
+const Post = ({post}:{post:FeedPostType}) => {
     return (
         <div className="flex flex-col gap-4">
             {/* USER */}
             <div className="flex items-center justify-between">
                 <div className="flex items-center gap-4">
-                    <Image src="https://images.pexels.com/photos/28818459/pexels-photo-28818459/free-photo-of-lush-green-forest-canopy-scene.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load" alt="" width={40} height={40} className="w-10 h-10 rounded-full"
+                    <Image src={post.user.avatar || "/noAvatar/png"} alt="" width={40} height={40} className="w-10 h-10 rounded-full"
                     />
-                    <span className="font-medium">John Doe</span>
+                    <span className="font-medium">{
+                        (post.user.name && post.user.surname) ? post.user.name + " " + post.user.surname : post.user.username
+                    }</span>
                 </div>
                 <Image src="/more.png" width={16} height={16} alt=""/>
             </div>
             {/* DESC */}
             <div className="flex flex-col gap-4">
-                <div className="w-full min-h-96 relative">
-                    <Image src="https://images.pexels.com/photos/28967156/pexels-photo-28967156/free-photo-of-scenic-wooden-bridge-in-a-lush-green-forest.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load" fill className="object-cover rounded-md" alt=""
+                {post.img && <div className="w-full min-h-96 relative">
+                    <Image src={post.img} fill className="object-cover rounded-md" alt=""
                     />
-                </div>
+                </div>}
                 <p>
-                    Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit. 
-                    Nullam eget urna ac nunc fermentum ultricies. 
-                    Cras nec eros et magna tincidunt fermentum. 
-                    Donec nec dui nec
+                    {post.desc}
                 </p>
             </div>
             {/* INTERACTION */}
